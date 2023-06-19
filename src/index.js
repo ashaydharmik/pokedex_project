3bb80a20547031b92d70fa991bec8822c93ef1eb
Ashay <101341591+ashaydharmik@users.noreply.github.com>
2023-06-19 22:39:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <app />
+    <App />
   </React.StrictMode>
 );
 
